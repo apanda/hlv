e51163910fa64afff37ed663cac268745a88be2e
Aurojit Panda <aurojit@gmail.com>
2013-09-21 18:29:18 -0700

    Move files around

--- client/src/cli.h
+++
@@ -1,9 +0,0 @@
-// Copyright 20xx The Regents of the University of California
-// This is a test service for SDN-v2 High Level Virtualization
-#include "server.h"
-#include "sync_client.h"
-#ifndef _HLV_CLIENT_REPL_H__
-#define _HLV_CLIENT_REPL_H_
-void repl (hlv::service::server::Server& server,
-           hlv::service::client::SyncClient& client);
-#endif
